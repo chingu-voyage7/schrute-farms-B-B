597f3c837eb2e47dc688191d7da47fc40279bc90
stufinn <30563561+finst@users.noreply.github.com>
2018-12-18 11:25:51 -0600

    Add server.js with routes

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const hbs = require('hbs');
+
+
+//this sets the port variable _either_ to the host machine's pre-specified port for node application (i.e. Heroku has this configured), OR to port 3000 if no port is already specified
+const port = process.env.PORT || 3000;
+
+//initializes an express server - BOOM!
+var app = express();
+
+
+
+// This tells the express app that we were using handlebars (i.e. hbs) to render the html to the server
+// there are a bunch of other rendering engines that we could use like pug (aka jade) etc.
+app.set('view engine', 'hbs');
+
+// this line tells the app where to look for static files (e.g. CSS and img files)
+// '__dirname' is the same as saying, "start at the root of this project folder for all relative file-paths that I specify below"
+app.use(express.static(__dirname));
+
+
+// --------  Establishing the "routes" ----------- //
+
+app.get('/', (req,res) => {
+  res.render('index.hbs');
+});
+
+app.get('/about', (req,res) => {
+  res.render('about.hbs');
+});
+
+app.get('/reservations', (req,res) => {
+  res.render('reservations.hbs');
+});
+
+app.get('/rooms', (req,res) => {
+  res.render('rooms.hbs');
+});
+
+app.listen(port, () => {
+  console.log(`Server is up and running on port ${port}`);
+});
