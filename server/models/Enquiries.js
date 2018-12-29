db8c72a4e4d1b5969cbec434ba9cb5e45a07da59
stufinn <30563561+finst@users.noreply.github.com>
2018-12-29 10:20:01 -0600

    Update contact.hbs to work properly and remove contacttest.hbs

--- server/models/Enquiries.js
+++ server/models/Enquiries.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 
 var ContactEnquiry = mongoose.model('ContactEnquiry', {
-  firstName: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  lastName: {
+  name: {
     type: String,
     required: true,
     minlength: 1,
     trim: true
   },
   email: {
+    type: String,
+    required: false,
+    trim: true
+  },
+  phone: {
     type: String,
     required: true,
     minlength: 3,
