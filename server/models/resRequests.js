283aa92b261f58edf915f98922bf121d6c39b399
stufinn <30563561+finst@users.noreply.github.com>
2018-12-29 12:10:31 -0600

    Add fields to ResRequest model

--- server/models/resRequests.js
+++ server/models/resRequests.js
@@ -18,9 +18,58 @@ var ResRequest = mongoose.model('ResRequest', {
   },
   email: {
     type: String,
-    required: false,
+    required: true,
     minlength: 3,
     trim: true
+  },
+  city: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  state: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  zip: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  agree: {
+    type: String,
+    required: true,
+  },
+  arrivalDate: {
+    type: String,
+    required: true
+  },
+  departureDate: {
+    type: String,
+    required: true
+  },
+  numAdults: {
+    type: Number,
+    required: true
+  },
+  numChildren: {
+    type: Number
+  },
+  numRooms: {
+    type: Number
+  },
+  room: {
+    type: String
+  },
+  flexibility: {
+    type: String
+  },
+  message: {
+    type: String
   }
 });
 
